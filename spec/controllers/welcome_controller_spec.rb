40b649e154b94542d582474538f31cd113f15d1e
Bruno Casali <brunoocasali@gmail.com>
2015-06-05 10:59:02 -0300

    fixed spec for welcome controller

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe WelcomeController, :type => :controller do
 
-  describe "GET index" do
-    it "returns http success" do
-      get :index
+  describe 'GET index' do
+    it 'returns http success' do
+      get :index, format: :json
+
       expect(response).to have_http_status(:success)
     end
   end
-
 end
