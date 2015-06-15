9cc1f8ae387afa55440c5bb86fda27ce1e239aa2
Bruno Casali <brunoocasali@gmail.com>
2015-06-15 17:34:44 -0300

    fixed person serializer to allow phone and email works well

--- app/serializers/person_serializer.rb
+++ app/serializers/person_serializer.rb
@@ -1,6 +1,6 @@
 class PersonSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :bio, :birthday_at, :fb_access_token, :tags,
-             :location, :status
+  attributes :id, :name, :email, :phone, :bio, :birthday_at, :fb_access_token,
+             :tags, :location, :status, :image
 
   def location
     LocationSerializer.new(object.location, scope: scope, root: false)
