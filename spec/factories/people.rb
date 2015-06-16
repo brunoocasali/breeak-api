ad55b88c79e532dda473b2d8408914eff7d7e3f1
Bruno Casali <brunoocasali@gmail.com>
2015-06-16 00:19:20 -0300

    fixed tests, added user_id to factories

--- spec/factories/people.rb
+++ spec/factories/people.rb
@@ -2,7 +2,9 @@ FactoryGirl.define do
   factory :person do
     bio { Forgery(:lorem_ipsum).text }
     name { Forgery(:name).full_name }
+    user_id { rand(999999999999) }
     email { Forgery(:email).address }
+    phone { Forgery(:address).phone }
     birthday_at { Forgery(:date).date }
     fb_access_token { Forgery(:basic).encrypt }
     tags %w(games sports ti)
@@ -14,6 +16,8 @@ FactoryGirl.define do
   factory :invalid_person, parent: :person do
     bio nil
     name nil
+    user_id nil
+    phone nil
     email nil
     birthday_at nil
     fb_access_token nil
